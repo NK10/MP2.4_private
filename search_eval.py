4635fb945bdf3a68f22ad593ee2dfa34d396f4c9
Nitin <nitin.kataria10@gmail.com>
2020-10-12 23:08:19 -0500

    d

--- search_eval.py
+++ search_eval.py
@@ -13,7 +13,7 @@ def load_ranker(cfg_file):
     """
     #return metapy.index.OkapiBM25()
     #return metapy.index.DirichletPrior()
-    return metapy.index.JelinekMercer(.5)
+    return metapy.index.JelinekMercer()
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: {} config.toml".format(sys.argv[0]))
